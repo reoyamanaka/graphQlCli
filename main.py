6db5818cb2e441ba590f38a32f13e7e7c5a807c9
Reo Yamanaka <ryamanaka807@gmail.com>
2021-06-21 09:12:19 -0400

    created all_users method

--- main.py
+++ main.py
@@ -10,8 +10,28 @@ class User(graphene.ObjectType, first = graphene.Int()):
 
 usersList = [
     User(first = "Reo", last = "Yamanaka", last_login = datetime.now()),
-    User(first = "Reo", last = "Yamanaka", last_login = datetime.now())
-    User(first = "Reo", last = "Yamanaka", last_login = datetime.now())
+    User(first = "Stacy", last = "Smith", last_login = datetime.now()),
+    User(first = "Livia", last = "Jackson", last_login = datetime.now())
 ]
 
+class Query(graphene.ObjectType):
+    all_users = graphene.List(User)
+    
+    def resolve_all_users(self, info):
+        return usersList
 
+schema = graphene.Schema(query = Query)
+
+result = schema.execute(
+    """
+    {
+        allUsers {
+            first
+            last
+            lastLogin
+        }
+    }
+    """
+)
+items = dict(result.data.items())
+print(json.dumps(items, indent = 4))
