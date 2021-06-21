e3ade528977ee2724230b90f9e1acf51a5d8d076
Reo Yamanaka <ryamanaka807@gmail.com>
2021-06-21 08:53:55 -0400

    created sample data and User class

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import graphene
 from datetime import datetime
 import json
+
+class User(graphene.ObjectType, first = graphene.Int()):
+    id = graphene.ID()
+    first = graphene.String()
+    last = graphene.String()
+    last_login = graphene.DateTime()
+
+usersList = [
+    User(first = "Reo", last = "Yamanaka", last_login = datetime.now()),
+    User(first = "Reo", last = "Yamanaka", last_login = datetime.now())
+    User(first = "Reo", last = "Yamanaka", last_login = datetime.now())
+]
+
+
